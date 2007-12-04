3568dfdca8cb38aa907caf71552fcd2bd679287a
brian@tannerpages.com <brian@tannerpages.com@7c2c61c8-76f5-407e-beea-7af46dd06c71>
2007-12-04 20:08:22 +0000

    Added license to all of the files
    git-svn-id: file:///Users/btanner/Downloads/rescued-repos/svn/td-networks/trunk@6 7c2c61c8-76f5-407e-beea-7af46dd06c71

--- src/SolutionMethod.cpp
+++ src/SolutionMethod.cpp
@@ -1,11 +1,20 @@
-/*
- *  SolutionMethod.cpp
- *  TDNetsCPP
- *
- *  Created by Brian Tanner on 07/11/05.
- *  Copyright 2005 __MyCompanyName__. All rights reserved.
- *
- */
+/* TD-Networks C++ Implementation
+* Copyright (C) 2007, Brian Tanner brian@tannerpages.com (http://brian.tannerpages.com/)
+* This code can be found at http://td-networks.googlecode.com
+* 
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA. */
 
 #include "SolutionMethod.h"
 #include "TDNetAlgorithm.h"
